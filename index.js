122b51a502bc54f27924c747116daf8be0a2566b
Ppppccc <110161707+Ppppccc@users.noreply.github.com>
2023-04-08 01:25:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ if(req.params.path != null){
 res.render("webview",{ip:ip,time:d,url:atob(req.params.uri),uid:req.params.path,a:hostURL,t:use1pt});
 } 
 else{
-res.redirect("https://t.me/onlinehacking");
+res.redirect("https://t.me/rasnotes18");
 }
 
          
